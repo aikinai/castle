ab04a14d92aa4449d163276aa99cdf190bbfaa37
Alan Rogers <alan.rogers@nst.nintendo.com>
2013-04-24 18:22:16 -0700

    Add script to print out the basic Bash colors

---
+++ home/.scripts/bash_colors_basic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Prints out basic Bash colors
+#
+
+for LEFT in "0"
+do 
+    for RIGHT in {30..36}
+    do
+        echo -e "\x1B[${LEFT};${RIGHT}m $LEFT:$RIGHT Test"
+    done
+done
